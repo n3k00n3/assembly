eedc676afc169efaa3a25c12955dc34e37a84e72
n3k00n3 <n3k00n3@protonmail.com>
2016-08-19 00:23:49 -0300

    skeleton

---
+++ x86_64/begin/skel.asm
@@ -0,0 +1,23 @@
+;---------------------------------------------------------------------
+;               This a simple skeleton of asm code
+; This file is a skeleton that can be used to start assembly programs.
+;---------------------------------------------------------------------
+
+global main
+
+section .text
+
+main:
+
+    ;code here
+
+section .data
+;
+; initialized data is put in the data segment here
+;
+
+
+section .bss
+;
+; uninitialized data is put in the bss segment
+;
