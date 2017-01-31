cbc447f3641bb86efbf277bd0aba109c09ab6d70
n3k00n3 <3mm4@riseup.net>
2017-01-30 22:49:06 -0300

    shift operations

--- x86_64/begin/shift_operations.asm
+++ x86_64/begin/shift_operations.asm
@@ -14,16 +14,27 @@ main:
     mov rbp, rsp
 
     xor rax, rax
-    mov ax, 0xa
-    shl ax, 0x1
-    shl ax, 0x1
-    shl ax, 0x2
-    
-    mov ax, 0x20
-    shr ax, 0x1
-    shr ax, 0x1
-    shr ax, 0x2
+    mov dl, 01001100b
+	mov cl, 1
+	shr dl, cl ;You can use cl or small numbers
+	;dl = 00100110b
+	;CF = 0
 
+	shr dl, 2
+	;dl = 00001001b
+	;CF = 1 
+
+	shl dl, 3
+	;dl = 01001000. You lose the last bit shifted
+	;CF == 0
+
+	mov al, 11001100b
+	sar al, 2
+	;al = 11110011b
+	;CF = 1
+	;used for signed numbers
+	;sal is another name to shl instruction
+	
     ;rotate shifts
     mov ax, 0xa
     rol ax, 0x1
