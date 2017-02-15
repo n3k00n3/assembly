3a9e9e81a08d1905df0e5e833f08d9a8244da06a
n3k00n3 <3mm4@riseup.net>
2017-02-15 13:41:29 -0300

    caesar code in asm

---
+++ x86_64/linux/begin/caesar.asm
@@ -0,0 +1,53 @@
+;---------------------------------------------------
+; First version - key = 1
+; usage: ./caesar < message.txt > encrypted_msg.txt
+;---------------------------------------------------
+
+[BITS 64]
+
+section .bss
+
+    buffer resb 1
+
+section .text
+
+global main:
+
+main:
+
+    push rbp
+    mov rbp, rsp
+
+readInput:
+    mov rax, 0x0
+    mov rdi, 0x0
+    mov rsi, buffer
+    mov rdx, 0x1
+    syscall
+
+    cmp rax, 0x0
+    je exit
+
+caesar:
+    
+    cmp byte [buffer], 0x41
+    jb write
+    cmp byte [buffer], 0x7a
+    ja write
+
+    add byte [buffer], dword [key]
+
+write:
+    mov rax, 0x1
+    mov rdi, 0x1
+    mov rsi, buffer
+    mov rdx, 0x1
+    syscall
+
+    jmp  readInput
+
+exit:
+    mov rsp, rbp
+    pop rbp
+    ret
+
